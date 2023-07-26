b9c0b1c59f81e7e035172afe9b3c42a4f5ed729f
Leladatan <vov4ik.samurai@yandex.ru>
2023-07-26 13:32:32 +0300

    ref

--- app/pro-players/components/ProPlayersList.tsx
+++ app/pro-players/components/ProPlayersList.tsx
@@ -11,8 +11,8 @@ const ProPlayersList: FC<ProPlayersListProps> = ({players}) => {
         <section className="grid 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-3 grid-cols-4 gap-5 px-6">
             {players?.map(player => (
                 <>
-                    <p key={player.account_id}>{player.name}</p>
-                    {player.avatarfull && <Image src={player.avatarfull} alt={player.personaname} />}
+                    <p key={player.account_id} className="text-center">{player.name}</p>
+                    {player.avatarfull && <Image src={player.avatarfull} width={50} height={50} alt={player.personaname} />}
                 </>
             ))}
         </section>
