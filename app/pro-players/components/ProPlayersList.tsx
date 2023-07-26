bd4e1a36f9cd99b6def6dbc47b98e42fdc7396d8
Leladatan <vov4ik.samurai@yandex.ru>
2023-07-26 13:36:15 +0300

    add style

--- app/pro-players/components/ProPlayersList.tsx
+++ app/pro-players/components/ProPlayersList.tsx
@@ -10,10 +10,10 @@ const ProPlayersList: FC<ProPlayersListProps> = ({players}) => {
     return (
         <section className="grid 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-3 grid-cols-4 gap-5 px-6">
             {players?.map(player => (
-                <>
-                    <p key={player.account_id} className="text-center">{player.name}</p>
+                <div key={player.account_id} className="flex flex-col items-center justify-center gap-y-2">
+                    <p className="text-center">{player.name}</p>
                     {player.avatarfull && <Image src={player.avatarfull} width={50} height={50} alt={player.personaname} loading="lazy" />}
-                </>
+                </div>
             ))}
         </section>
     );
